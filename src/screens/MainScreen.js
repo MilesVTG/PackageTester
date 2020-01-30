111e9ff634bff9d364830a78eb399ffd5d8ea104
MilesVTG <mchick@vtg-services.com>
2020-01-30 16:07:01 -0600

    store test2

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -2,12 +2,23 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import styles from '../../styles/mainStyles';
 import {useSelector, useDispatch} from 'react-redux';
+import {Button} from 'react-native-elements';
+import {TOGGLE_LOGIN} from '../../store/actions/actionTypes';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const MainScreen = props => {
   const userName = useSelector(state => state.items.test);
+
+  const dispatch = useDispatch();
+  //   console.log(toggleLogin);
   return (
     <View style={styles.screen}>
       <Text>The Main Screen {userName}</Text>
+      <Button
+        onPress={() => dispatch({type: TOGGLE_LOGIN})}
+        title=" Hello Moe"
+        icon={<Icon name="terminal" size={26} color="white" />}
+      />
     </View>
   );
 };
